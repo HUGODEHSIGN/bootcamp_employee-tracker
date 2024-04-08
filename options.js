d35ce1a6a47ac5c854515917081f2a8607ef638c
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-08 00:36:27 -0400

    added change role functionality

--- options.js
+++ options.js
@@ -1,5 +1,6 @@
 import viewAllEmployees from './options/viewAllEmployees.js';
 import addEmployee from './options/addEmployee.js';
+import updateEmployeeRole from './options/updateEmployeeRole.js';
 
 function placeholder() {
   console.log('here');
@@ -8,7 +9,7 @@ function placeholder() {
 const optionsLookup = {
   'View All Employees': viewAllEmployees,
   'Add Employee': addEmployee,
-  'Update Employee Role': placeholder,
+  'Update Employee Role': updateEmployeeRole,
   'View All Roles': placeholder,
   'Add Role': placeholder,
   'View All Departments': placeholder,
---
+++ options/updateEmployeeRole.js
@@ -0,0 +1,49 @@
+import pool from '../server.js';
+import inquirer from 'inquirer';
+export default async function updateEmployeeRole() {
+  try {
+    const { rows: employees } =
+      await pool.query(`SELECT id, CONCAT(first_name, ' ', last_name) AS name
+          FROM employee;`);
+    const { rows: roles } = await pool.query(`SELECT id, title FROM role;`);
+
+    const employeesArray = employees.map(({ name }) => name);
+    const rolesArray = roles.map(({ title }) => title);
+
+    const { employee, role } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'employee',
+        message: "What is the employee's role?",
+        choices: employeesArray,
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: "Who is the employee's manager?",
+        choices: rolesArray,
+      },
+    ]);
+
+    const employeeId = employees.filter(({ name }) => name === employee)[0].id;
+    const roleId = roles.filter(({ title }) => title === role)[0].id;
+
+    pool.query(
+      `UPDATE employee
+    SET role_id = $1
+    WHERE id = $2;`,
+      [roleId, employeeId],
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Updated employee's role");
+      }
+    );
+
+    console.log(employeeId, roleId);
+  } catch (err) {
+    console.error(err);
+  }
+}
