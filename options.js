6d107693885819b93dda745e14bfaebcd2e4111a
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-07 22:04:07 -0400

    added an options lookup for main inquirer prompt

---
+++ options.js
@@ -0,0 +1,11 @@
+function placeholder() {
+  console.log('here');
+}
+
+const optionsLookup = {
+  'View All Employees': placeholder,
+  'Add Employee': placeholder,
+  'Update Employee Role': placeholder,
+};
+
+export default optionsLookup;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import pgk from 'pg';
+import inquirer from 'inquirer';
+import optionsLookup from './options.js';
 const { Pool } = pgk;
 
 const PORT = process.env.PORT || 3001;
@@ -19,6 +21,15 @@ const pool = new Pool(
 );
 
 pool.connect();
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'options',
+    message: 'What would you like to do?',
+    choices: Object.keys(optionsLookup),
+  },
+]);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:3001/`);
